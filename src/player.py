9cccc60d358117002b6e2ce9f23e5224068c46dc
Creativelad <creative.lad.20@gmail.com>
2025-05-09 23:50:03 +0530

    movement fix but better

--- src/player.py
+++ src/player.py
@@ -4,14 +4,20 @@ class Player:
         self.x=x
         self.y=y
         self.image = image
+        self.direction=True
     def move(self,dx,dy):
         self.x += dx
         self.y += dy
-        image = pygame.image.load('../assets/player/cat.png')
-        image = pygame.transform.scale(image, (round(image.get_width()/2),round(image.get_height()/2)))
+        #image = pygame.image.load('../assets/player/cat.png')
+        #image = pygame.transform.scale(image, (round(image.get_width()/2),round(image.get_height()/2)))
 
-        if dx>0:
-            self.image =  pygame.transform.flip(image, True, False)
-        if dx<0:
-             self.image = image
+        #if dx>0:
+        #self.image =  pygame.transform.flip(image, True, False)
+        #if dx<0:
+        # self.image = image
+        if dx==0 :
+            return
+        if (dx<0) != self.direction: 
+            self.image=pygame.transform.flip(self.image,True,False)
+            self.direction=not self.direction
     

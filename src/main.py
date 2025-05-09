0dc9d6ea242b694d3eb000ede7eca2f865b05c0a
Creativelad <creative.lad.20@gmail.com>
2025-05-09 15:02:05 +0530

    Better Structure

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((400, 300),pygame.RESIZABLE)
 pygame.display.set_caption('Pygame Test')
-
+screen.fill((30, 30, 46))  
+pygame.display.flip()  
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
-    screen.fill((30, 30, 46))  
-    pygame.display.flip()  
+    
 
 pygame.quit()

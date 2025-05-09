ca0c8336fcf3817e7634e9d2faeaf816574e7c8d
Creativelad <creative.lad.20@gmail.com>
2025-05-09 21:54:59 +0530

    OOP

--- src/game.py
+++ src/game.py
@@ -1,29 +1,29 @@
 import pygame
 import sys
+from player import Player
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.cat = pygame.image.load('../assets/player/cat.png')
-        self.cat= pygame.transform.scale(self.cat, (round(self.cat.get_width()/2),round(self.cat.get_height()/2)))
-        self.x = 100
-        self.y=100
 
     def run(self):
+        cat_image = pygame.image.load('../assets/player/cat.png')
+        cat_image = pygame.transform.scale(cat_image, (round(cat_image.get_width()/2),round(cat_image.get_height()/2)))
+        cat = Player(100,100,cat_image)
         while self.running:
             self.screen.fill((30, 30, 46))
-            self.screen.blit(self.cat,(self.x,self.y))
+            self.screen.blit(cat.image,(cat.x,cat.y))
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
-                self.y-=5
+                cat.y-=5
             if keys[pygame.K_s]:
-                self.y+=5
+                cat.y+=5
             if keys[pygame.K_d]:
-                self.x+=5
+                cat.x+=5
             if keys[pygame.K_a]:
-                self.x-=5
+                cat.x-=5
             for event in pygame.event.get():
                  if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     pygame.quit()
---
+++ src/player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self,x,y,image):
+        self.x=x
+        self.y=y
+        self.image = image
+    def move(self,dx,dy,image):
+        self.x +=dx
+        self.y +=dy
+        self.image = image
+    

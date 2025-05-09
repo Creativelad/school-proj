0d9ddfd68f598b482a05e4576199bd0da8cbbd57
Creativelad <creative.lad.20@gmail.com>
2025-05-09 15:27:21 +0530

    guh

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
+import sys
 import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 300),pygame.RESIZABLE)
-pygame.display.set_caption('Pygame Test')
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Pygame Game')
 screen.fill((30, 30, 46))  
 pygame.display.flip()  
 running = True
+clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            running = False
+            pygame.quit()
+            sys.exit()
+    clock.tick()
 
     
 
